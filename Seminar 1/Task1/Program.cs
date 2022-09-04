46d92d77d3f542f2ecd7a00873bc8fa6cfe4e9bd
Igoremail <12345>
2022-09-04 16:21:41 +0300

    task 1

--- Seminar 1/Task1/Program.cs
+++ Seminar 1/Task1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+
+int value = Prompt("Введите число -> ");
+if (value>99 && value <1000)
+{
+    int result = value/10%10;
+    System.Console.WriteLine(result);
+}
+else
+{
+    System.Console.WriteLine("Число неверное");
+}
