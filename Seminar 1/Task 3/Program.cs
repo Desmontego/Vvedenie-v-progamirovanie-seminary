6bad239456063876caea89333abb2b28c5024b95
Igoremail <12345>
2022-09-06 19:51:24 +0300

    Готовое домашнее

---
+++ Seminar 1/Task 3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+int value = Prompt("Введите число -> ");
+if (value<100)
+{
+    System.Console.WriteLine("нет третьего числа");
+}
+else
+{
+    int result = value/10%10;
+    System.Console.WriteLine(result);
+}   
---
+++ Seminar 1/Task 4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 
+// int Prompt(string message)
+// {
+//     Console.Write(message);
+//     string strValue = Console.ReadLine() ?? "0";
+//     int value = int.Parse(strValue);
+//     return value;
+// }
+
+// int value = Prompt("Введите число -> ");
+Console.WriteLine ("Введите первое число");
+int value =int.Parse(Console.ReadLine()!);
+bool remains = (value=>1) && (value <=8);
+if (remains)
+{
+    System.Console.WriteLine("НЕТ");
+}
+else
+{
+    System.Console.WriteLine("Да");    
+}
