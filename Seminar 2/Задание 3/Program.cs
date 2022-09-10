1e02b6ac0e8c51d89662d24f39e7907feb22a967
Igoremail <12345>
2022-09-10 13:05:52 +0300

    3

---
+++ Seminar 2/Задание 3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел о
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+
+int number = Prompt("Введите число ->");
+void Coub (int number)
+{
+    int index =1;
+    while (index <= number)
+    {
+        System.Console.WriteLine($"Квадрат числа {index} -> {index * index*index}");
+        index++;
+    }
+}
+Coub(number);
