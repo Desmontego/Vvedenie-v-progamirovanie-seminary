fff30d97332b345e2ee5dddea20494a00a76ca4c
Igoremail <12345>
2022-09-18 09:27:49 +0300

    task 4

---
+++ Seminar 4/Task 2/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int[] array = new int[10];
+int summ = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 10);
+    if (array[i] % 2 == 1)
+    {
+        summ +=array[i];
+    }
+    System.Console.Write(array[i]+ " ");
+}
+System.Console.WriteLine();
+System.Console.WriteLine(summ);
---
+++ Seminar 4/Task 3/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Задайте массив вещественных чисел.
+//  Найдите разницу между максимальным и минимальным элементов массива.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int[] array = new int[10];
+        int max = 0, min = 10;
+        for (int i = 0; i < array.Length; i++) array[i] = rnd.Next(1, 10);
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > max) max = array[i];
+            if (array[i] < min) min = array[i];
+        }
+
+        for (int i = 0; i < array.Length; i++) Console.Write(" " + array[i]);//выводим массив
+        Console.WriteLine();
+        Console.WriteLine("Разница максимального и минимального:  " + (max - min));
+    }
+}
