b678ac4ab6a54dbf157bc58fdc733e61557a160b
Igoremail <12345>
2022-10-05 22:15:31 +0700

    Lesson 7

---
+++ Seminar 7/Task 1/Program.cs
@@ -0,0 +1,34 @@
+﻿//  Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+int linesVol = 3;
+int columnsVol = 3;
+int[,] numbers = new int[linesVol, columnsVol];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine();
+Console.WriteLine("Массив");
+PrintArray(numbers);
+
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
